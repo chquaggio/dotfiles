f5a2bddecda4e907cc9fa069a4de153797ca14c5
chquaggio <quaggio.christian@gmail.com>
2025-10-04 10:24:11 +0200

    fix: restore nvim config

---
+++ personal/config/nvim/lua/lsp/pylsp.lua
@@ -0,0 +1,26 @@
+---@type vim.lsp.Config
+return {
+  cmd = { "pylsp" },
+  filetypes = { "python" },
+  root_markers = {
+    "pyproject.toml",
+    "setup.py",
+    "setup.cfg",
+    "requirements.txt",
+    "Pipfile",
+    ".git",
+  },
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = { "W391", "W503" },
+          maxLineLength = 136,
+        },
+        -- pyflakes = {
+        --   enabled = false,
+        -- },
+      },
+    },
+  },
+}
